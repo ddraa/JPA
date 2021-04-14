81ba6ee5fda27df7267611954befecf9fb9672f5
Dragon <ruuddyd@gmail.com>
2021-04-14 22:33:08 +0900

    회원 서비스 개발

---
+++ src/main/java/book/shop/service/MemberService.java
@@ -0,0 +1,54 @@
+package book.shop.service;
+
+import book.shop.domain.Member;
+import book.shop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // auto DI only 'final' field
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+    public MemberService(MemberRepository memberRepository) { // 생성자 주입
+        this.memberRepository = memberRepository;
+    }
+     **/
+
+    /**
+     회원 가입
+     **/
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("already exist");
+        }
+
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
