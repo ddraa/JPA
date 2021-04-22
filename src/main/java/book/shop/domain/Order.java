6316bb61a0c8441c34e04a2e69ad2a69d9750e6c
Dragon <ruuddyd@gmail.com>
2021-04-22 20:20:47 +0900

    mod

--- src/main/java/book/shop/domain/Order.java
+++ src/main/java/book/shop/domain/Order.java
@@ -1,9 +1,12 @@
 package book.shop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.nio.channels.IllegalChannelGroupException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -23,6 +27,10 @@ public class Order {
     @JoinColumn(name = "member_id") // foreign key
     private Member member;
 
+    /**
+     * cascade .. order persist 되면 자동으로 order 엔티티에 있는 orderItems와 delivery도 같이 persist 일어난다.
+     * 그래서 orderitems와 delivery 엔티티 별개로 persisi 안해도 된다.
+     */
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
@@ -49,5 +57,44 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 비즈니스 로직
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) { // this.orderItems..
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) totalPrice += orderItem.getTotalPrice();
+        return totalPrice;
     }
 }
